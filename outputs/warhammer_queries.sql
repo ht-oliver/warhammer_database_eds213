72b4edea639ac6d131ab49b901a723183203f07b
ht-oliver <holiver327@gmail.com>
2026-05-12 23:36:20 -0700

    Added comments to SQL

--- outputs/warhammer_queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM datasheets;
-
-SELECT * FROM model_cost;
-
-SELECT * FROM factions;
-
-SELECT * FROM models;
-
--- Which faction has the most models for sale
--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
---- Tables
--- datasheets
--- I want to find out which faction has the most affordable models
--- for each size of model
+
+-- I want to find out which faction has the most affordable models for each size of model
 SELECT 
     f.faction_name AS faction,
     m.size,
-    ROUND(AVG(mc.cost), 2) AS avg_cost
-FROM datasheets d
-JOIN models m ON d.model_id = m.model_id
-JOIN model_cost mc ON d.model_id = mc.model_id
-JOIN factions f ON d.faction_id = f.faction_id
-WHERE m.size IS NOT NULL
-GROUP BY f.faction_name, m.size
-ORDER BY f.faction_name, m.size;
+    ROUND(AVG(mc.cost), 2) AS avg_cost -- Find the average cost of models, round to two decimals
+FROM datasheets d -- Join from datasheets, this is our primary table
+JOIN models m ON d.model_id = m.model_id -- Join on model_id, the primary key
+JOIN model_cost mc ON d.model_id = mc.model_id -- Join on model_cost to get costs
+JOIN factions f ON d.faction_id = f.faction_id -- Join to factions to get faction names
+WHERE m.size IS NOT NULL -- Remove observations with no size value
+GROUP BY f.faction_name, m.size -- Group by faction name, then by size to get model sizes per faction
+ORDER BY f.faction_name, m.size; -- Order by faction name, and then small to large. Put some order to the chaos
+
+
